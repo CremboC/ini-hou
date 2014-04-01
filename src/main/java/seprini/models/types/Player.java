7eb7fef5c98cc8c6562ff56c4416b397212414a9
pauliusi <paulius.imbrasas@metasite.net>
2014-04-01 22:07:35 +0100

    #12 Comments

--- src/main/java/seprini/models/types/Player.java
+++ src/main/java/seprini/models/types/Player.java
@@ -2,6 +2,13 @@ package seprini.models.types;
 
 import com.badlogic.gdx.Input.Keys;
 
+/**
+ * Sets all of the controls for the different players. Passed to aircraft to
+ * later get the correct controls
+ * 
+ * @author Crembo
+ * 
+ */
 public class Player {
 
 	public final static int ONE = 0;
@@ -9,6 +16,8 @@ public class Player {
 
 	public int LEFT, RIGHT, ALT_INC, ALT_DEC, SPEED_INC, SPEED_DEC,
 			RETURN_TO_PATH, SWITCH_PLANE;
+
+	public int number;
 
 	public Player(int playerNumber) {
 		switch (playerNumber) {
@@ -24,6 +33,8 @@ public class Player {
 
 			RETURN_TO_PATH = Keys.R;
 			SWITCH_PLANE = Keys.TAB;
+
+			number = Player.ONE;
 			break;
 
 		case TWO:
@@ -38,39 +49,91 @@ public class Player {
 
 			RETURN_TO_PATH = Keys.NUM_0;
 			SWITCH_PLANE = Keys.NUM_5;
+
+			number = Player.TWO;
 			break;
 		}
 	}
 
+	/**
+	 * Get key for turning left
+	 * 
+	 * @return
+	 */
 	public int getLeft() {
 		return LEFT;
 	}
 
+	/**
+	 * Get key for turning right
+	 * 
+	 * @return
+	 */
 	public int getRight() {
 		return RIGHT;
 	}
 
+	/**
+	 * Get key to increase altitude
+	 * 
+	 * @return
+	 */
 	public int getAltIncrease() {
 		return ALT_INC;
 	}
 
+	/**
+	 * Get key to decrease altitude
+	 * 
+	 * @return
+	 */
 	public int getAltDecrease() {
 		return ALT_DEC;
 	}
 
+	/**
+	 * Get key to increase speed
+	 * 
+	 * @return
+	 */
 	public int getSpeedIncrease() {
 		return SPEED_INC;
 	}
 
+	/**
+	 * Gey key to decrease speed
+	 * 
+	 * @return
+	 */
 	public int getSpeedDecrease() {
 		return SPEED_DEC;
 	}
 
+	/**
+	 * Get key to return to path (restore aircraft's route after taking direct
+	 * control)
+	 * 
+	 * @return
+	 */
 	public int getReturnToPath() {
 		return RETURN_TO_PATH;
 	}
 
+	/**
+	 * Get key to switch between aircraft
+	 * 
+	 * @return
+	 */
 	public int getSwitchPlane() {
 		return SWITCH_PLANE;
+	}
+
+	/**
+	 * Gets which player this is - one or two
+	 * 
+	 * @return
+	 */
+	public int getNumber() {
+		return number;
 	}
 }
